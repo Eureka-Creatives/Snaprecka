2c1a58ec7ca893804d0f9ba509d200283ea0f17d
Opedepodepes-Olugbemi <olugbemiopedepo@gmail.com>
2025-05-03 19:31:17 +0100

    feat: login screen route

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import UserLayout from "./layout/userLayout";
 import UserHome from "./pages/user-home";
-
+import Login from "./pages/login-screen";
 export const router = createBrowserRouter(
   [
+    {
+      path: "/",
+      Component: Login,
+    },
     {
       path: "/home",
       Component: UserLayout,
