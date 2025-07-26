7bfeb222d12234362384d3fdc47cf7714c0209aa
Thoobi <sydneydaniel76@gmail.com>
2025-07-26 19:35:48 +0100

    fix: this is a fix up on the UI

--- frontend/src/components/user/navbar.tsx
+++ frontend/src/components/user/navbar.tsx
@@ -1,77 +1,33 @@
 import { CgSearch } from "react-icons/cg";
 import { FiPlus } from "react-icons/fi";
-import VaulDrawer from "./drawer";
-import { useState } from "react";
-import { Drawer } from "vaul";
 import { Button } from "../ui/button";
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <nav className="py-2.5 w-full max-md:px-3 flex flex-row justify-between items-center font-dm text-black bg-white">
+    <nav className="py-7.5 w-full max-md:px-3 pr-15 flex flex-row justify-between items-center font-dm text-black bg-white border-2 border-red-400">
       <div className="">
-        <h1 className="text-2xl max-md:text-xl font-bold text-black">angles</h1>
+        <h1 className="text-3xl max-md:text-xl font-bold text-black p-2.5">
+          Dashboard
+        </h1>
       </div>
-      <div className="flex flex-row items-center justify-between w-full max-md:gap-8">
-        {/* <div className="flex items-center gap-2 cursor-pointer border border-soft-blue/50 px-4 py-1 rounded-full hover:bg-soft-blue/5 transition-colors duration-100 ease-out">
-          <span className="text-base max-md:text-sm">Capsules</span>
-          <GoChevronDown className="text-lg max-md:text-base" />
-        </div> */}
-        <ul className="flex items-center gap-4 max-md:gap-1 w-full justify-end">
-          <li className="cursor-pointer bg-soft-blue/15 px-3 py-2 flex items-center gap-2 rounded-full transition-colors duration-100 ease-out max-md:hidden">
-            <div>
-              <input
-                type="text"
-                placeholder="Search for a capsule"
-                className="bg-transparent outline-none text-sm max-md:text-sm px-2 w-full"
-              />
-            </div>
+      <ul className="flex items-center justify-between border-2 border-green-400 w-1/2">
+        <li className="cursor-pointer bg-[#737791] flex items-center gap-2 max-md:hidden">
+          <div className="w-full">
             <CgSearch className="text-base text-gray-600" />
-          </li>
-          <li className="cursor-pointer">
-            <Button
-              variant="ghost"
-              className="text-sm h-9 font-light bg-soft-blue/15 hover:bg-soft-blue/10 px-5 rounded-full flex items-center gap-2 text-gray-600"
-            >
-              <span className="max-md:hidden">Create</span>
-              <FiPlus className="text-base max-md:text-sm font-light" />
-            </Button>
-          </li>
-          <li className="border-soft-blue cursor-pointer border rounded-full p-1 hidden max-lg:block h-8 w-8">
-            <VaulDrawer setIsOpen={setIsOpen} isOpen={isOpen}>
-              <div className="p-4 bg-white rounded-t-[10px] flex-1 font-dm">
-                <div
-                  aria-hidden
-                  className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 mb-8"
-                />
-                <div className="max-w-md mx-auto">
-                  <Drawer.Title className="font-medium mb-4 text-gray-900">
-                    Drawer for React.
-                  </Drawer.Title>
-                  <p className="text-gray-600 mb-2">
-                    This component can be used as a Dialog replacement on mobile
-                    and tablet devices. You can read about why and how it was
-                    built{" "}
-                    <a
-                      target="_blank"
-                      className="underline"
-                      href="https://emilkowal.ski/ui/building-a-drawer-component"
-                    >
-                      here
-                    </a>
-                    .
-                  </p>
-                  <p className="text-gray-600 mb-2">
-                    This one specifically is the most simplest setup you can
-                    have, just a simple drawer with a trigger.
-                  </p>
-                </div>
-              </div>
-            </VaulDrawer>
-          </li>
-        </ul>
-      </div>
+            <input
+              type="text"
+              placeholder="Search for a capsule"
+              className="bg-transparent outline-none text-sm max-md:text-sm px-2"
+            />
+          </div>
+        </li>
+        <li className="cursor-pointer">
+          <Button className="text-sm h-9 font-light flex items-center gap-2 bg-white border-2 border-soft-blue text-gray-600">
+            <span className="max-md:hidden">Create</span>
+            <FiPlus className="text-base max-md:text-sm font-light" />
+          </Button>
+        </li>
+      </ul>
     </nav>
   );
 }
--- frontend/src/layout/userLayout.tsx
+++ frontend/src/layout/userLayout.tsx
@@ -6,10 +6,10 @@ export default function UserLayout() {
   return (
     <main className="flex flex-col items-stretch min-w-screen min-h-screen font-dm container mx-auto  bg-white relative max-md:px-0">
       <div className="flex-1 w-full flex flex-row gap-2 top-14 h-full z-40">
-        <div className="w-[20%] max-lg:hidden border-2 border-blue-400">
+        <div className="w-[20%] max-lg:hidden">
           <AppSidebar />
         </div>
-        <div className="w-[82%] max-lg:w-full border-2 border-red-400 flex flex-col gap-8 bg-[#F9FAFB]">
+        <div className="w-[82%] max-lg:w-full flex flex-col gap-8 bg-[#F9FAFB]">
           <Navbar />
           <Outlet />
         </div>
