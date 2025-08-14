48da3364643f78303ecea9a2c209ed7ed0caaddd
Thoobi <sydneydaniel76@gmail.com>
2025-08-14 01:23:38 +0100

    refactor: simplify LandingPage component and remove unused imports feat: integrate Analytics component in main application render

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,13 +1,5 @@
-// import One from "../assets/landing-page/one.png";
-// import Two from "../assets/landing-page/two.png";
-// import Three from "../assets/landing-page/three.jpg";
-// import { Button } from "@/components/ui/button";
-import Hero from "@/components/landing/hero";
+import React from "react";
 
 export default function LandingPage() {
-  return (
-    <section>
-      <Hero />
-    </section>
-  );
+  return <div>Hii</div>;
 }
