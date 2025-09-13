03f20c140baaeeae14caa494e7366e735e0b7840
Thoobi <sydneydaniel76@gmail.com>
2025-09-13 20:34:54 +0100

    chores: testing for the feature section

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/landing/hero";
-import Feature from "@/components/landing/feature";
+// import Feature from "@/components/landing/feature";
 import How from "@/components/landing/how";
 import CallToAction from "@/components/landing/call";
 
@@ -7,7 +7,7 @@ export default function LandingPage() {
   return (
     <section className="w-full min-h-screen">
       <Hero />
-      <Feature />
+      <How />
       <How />
       <CallToAction />
     </section>
